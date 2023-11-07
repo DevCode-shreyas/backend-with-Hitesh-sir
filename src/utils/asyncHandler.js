98e4cc41776c8b68f3784c8f8605f2214a196aa8
shreyas bole <shreyasbole4@gmail.com>
2023-11-07 23:23:54 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// production grade code
+
+const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(500).json({
+//       message: error.message,
+//       success: false,
+//     });
+//   }
+// };
